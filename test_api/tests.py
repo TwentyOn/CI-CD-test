7e1f5be5a853db03ab11862e149bdc7dfa76a40d
Alexey <aleksey-000@mail.ru>
2026-05-12 18:44:53 +0500

    add: базовый тест

--- test_api/tests.py
+++ test_api/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from .views import TestView
+
 
 # Create your tests here.
+class TestTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_test_view(self):
+        url = reverse('test')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('message', response.data)
+        self.assertEqual(response.data['message'], 'success')
--- test_api/urls.py
+++ test_api/urls.py
@@ -3,5 +3,5 @@ from rest_framework.urls import path
 from .views import TestView
 
 urlpatterns = [
-    path('', TestView.as_view())
+    path('', TestView.as_view(), name='test')
 ]
