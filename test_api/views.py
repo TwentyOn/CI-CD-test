2c75ea85ddc98adc9e6f0f5358dd038651e24303
Alexey <aleksey-000@mail.ru>
2026-05-12 18:11:02 +0500

    изменение

--- test_api/views.py
+++ test_api/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
 # Create your views here.
 class TestView(APIView):
     def get(self, request):
